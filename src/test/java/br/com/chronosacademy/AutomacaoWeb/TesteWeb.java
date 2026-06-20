4ba79e60730d4586568a89e00ddb6486ccc31d1a
Mateus Camilo <mc.rodrigues12m@gmail.com>
2026-06-20 13:40:05 -0300

    Adcionando logica web

---
+++ src/test/java/br/com/chronosacademy/AutomacaoWeb/TesteWeb.java
@@ -0,0 +1,40 @@
+package br.com.chronosacademy.AutomacaoWeb;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class TesteWeb {
+
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void incializaTeste() {
+        driver.manage().window().maximize();
+        driver.get("https://www.youtube.com");
+
+
+        String xPathTitulo = "//div[2]/h2";
+        WebElement xPathTxt = driver.findElement(By.xpath(xPathTitulo));
+        String titulo = xPathTxt.getText();
+        assertEquals("Que tal uma pesquisa para começar?", titulo);
+    }
+
+    @After
+    public void tearDown() {
+    driver.quit();
+    }
+}
